b07ec36083f66edfe1cf5355629b6bb00d20a366
mapomatic <mapomatic.waze@gmail.com>
2021-06-03 08:50:30 -0400

    Update WME-FC-Layer.js

--- WME-FC-Layer.js
+++ WME-FC-Layer.js
@@ -8,7 +8,7 @@
 // // ==UserScript==
 // @name         WME FC Layer
 // @namespace    https://greasyfork.org/users/45389
-// @version      2021.02.19.001
+// @version      2021.06.03.001
 // @description  Adds a Functional Class layer for states that publish ArcGIS FC data.
 // @author       MapOMatic
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
